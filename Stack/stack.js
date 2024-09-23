167e46e22c34f61907c8785cd2ceae52bc9c3f9c
preethamsetty <preethambs2002@gmail.com>
2024-09-23 18:13:23 +0530

    [Preetham B S] add - added Stack folder

---
+++ Stack/stack.js
@@ -0,0 +1,21 @@
+const myStack = new Stack();
+
+myStack.push(10);
+myStack.push(20);
+myStack.push(30);
+
+console.log(myStack.peek()); // Output: 30
+console.log(myStack.pop()); // Output: 30
+console.log(myStack.peek()); // Output: 20
+
+//-------------------------------------------------
+
+const myStack1 = new Stack();
+
+myStack.push(10);
+myStack.push(20);
+myStack.push(30);
+
+console.log(myStack1.peek()); // Output: 30
+console.log(myStack1.pop()); // Output: 30
+console.log(myStack1.peek()); // Output: 20
