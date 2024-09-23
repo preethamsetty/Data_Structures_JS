b41ad345adf04c3ffa93f8aabcb4414b19b9d35f
preethamsetty <preethambs2002@gmail.com>
2024-09-23 18:14:10 +0530

    [Preetham B S] add - added Strings folder

---
+++ Strings/strings.js
@@ -0,0 +1,13 @@
+const str = "Hello, world!";
+
+console.log(str.length); // Output: 13
+
+console.log(str.charAt(0)); // Output: H
+
+console.log(str.indexOf("world")); // Output: 7
+
+console.log(str.slice(0, 5)); // Output: Hello
+
+console.log(str.toUpperCase()); // Output: HELLO, WORLD!
+
+console.log(str.split(" ")); // Output: ["Hello,", "world!"]
